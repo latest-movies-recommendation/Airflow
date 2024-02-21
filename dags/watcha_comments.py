7459df5cc14ec803032973485140f786fdf7e526
bbbbeee11a <bbbbeeella@jbnu.ac.kr>
2024-02-21 13:00:37 +0900

    refactor: modified DAG file

--- dags/watcha_comments.py
+++ dags/watcha_comments.py
@@ -1,127 +1,197 @@
-from airflow import DAG
-from airflow.models import Variable
-from airflow.operators.python import PythonOperator
-from airflow.hooks.S3_hook import S3Hook
-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
-
-from datetime import datetime, timedelta
-import time
-import pandas as pd
-import boto3
 import logging
+import re
+import time
+from datetime import datetime, timedelta
 from io import StringIO
 
+import pandas as pd
+from airflow import DAG
+from airflow.hooks.S3_hook import S3Hook
+from airflow.models import Variable
+from airflow.operators.python import PythonOperator
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
-def get_titles(ti, **kwargs):
+
+# 오늘 날짜 ex)20240216
+def today_date():
+    now = datetime.now()
+    return now.strftime("%Y%m%d")
+
+
+def get_daily_box_office(ti, **kwargs):
     s3_hook = S3Hook(aws_conn_id='aws_conn')
-    s3_key = 'kofic/daily-box-office/20240216.csv'
+    s3_key = f'kofic/daily-box-office/{today_date()}.csv'
     try:
         obj = s3_hook.get_key(key=s3_key, bucket_name=Variable.get('s3_bucket_name'))
         if obj:
             # CSV 파일 데이터를 Pandas DataFrame으로 읽어오기
             csv_data = obj.get()['Body'].read().decode('utf-8')
             df = pd.read_csv(StringIO(csv_data))
-            logging.info("CSV file downloaded and converted to DataFrame successfully.")
+            logging.info(f"{today_date()}자 daily-box-office 파일 다운로드 및 데이터프레임으로의 변환 성공!")
             if 'movieNm' not in df.columns:
-                raise ValueError("movieNm Column does not exist in the dataframe.")
+                raise ValueError("movieNm 컬럼이 데이터프레임에 존재하지 않습니다.")
             # 영진위 1-10위 영화
             movies = df['movieNm'].tolist()[-10:]
             ti.xcom_push(key='movies_title', value=movies)
-            logging.info(movies)
+            logging.info(f"{today_date()}자 박스오피스 순위: {movies}")
             return movies
         else:
-            logging.info("File not found in S3.")
+            logging.info(f"S3에 {today_date()}자 해당 파일이 없습니다.")
             return None
     except Exception as e:
-        logging.info(f"Data load failed from s3: {e}")
+        logging.info(f"S3로부터 데이터를 로드하는 데 실패했습니다.: {e}")
 
 
-def access_watcha(title, **kwargs):
-    try:
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_experimental_option("detach", True)
+def scraping_watcha(**kwargs):
+    # ti = kwargs['ti']
+    # titles = ti.xcom_pull(task_ids='get_daily_box_office', key='movies_title')
+    titles = ["시시콜콜한 이야기", "애프터썬"]
+    logging.info(titles)
 
-        driver = webdriver.Chrome('/opt/homebrew/bin/chromedriver', options=chrome_options)
-        driver.set_page_load_timeout(30)
-
-        url = f"https://pedia.watcha.com/ko-KR/search?query={title}"
-        driver.get(url)
-        time.sleep(3)
-        logging.info(f"{title}으로 Watcha에 접근중")
-        return driver
-    except Exception as e:
-        logging.info(f"Fail to Access Watcha: {e}")
-    # df = comments(title, driver)
-    # print(df)
-
-def actual_access_watcha_logic(title, **kwargs):
-    try:
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_experimental_option("detach", True)
-        driver = webdriver.Chrome('/opt/homebrew/bin/chromedriver', options=chrome_options)
-        driver.set_page_load_timeout(30)
-        url = f"https://pedia.watcha.com/ko-KR/search?query={title}"
-        driver.get(url)
-        time.sleep(3)
-        logging.info(f"{title}으로 Watcha에 접근중")
-        # 여기서 필요한 작업 수행...
-        return driver
-    except Exception as e:
-        logging.info(f"Fail to Access Watcha: {e}")
-
-
-def access_watcha_wrapper(**kwargs):
-    ti = kwargs['ti']
-    titles = ti.xcom_pull(task_ids='get_titles', key='movies_title')
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('window-size=1200x600')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    s = Service('/usr/bin/chromedriver')
+    driver = webdriver.Chrome(service=s, options=options)
     if titles is not None:
         for title in titles:
-            actual_access_watcha_logic(title, **kwargs)
+            logging.info(f"{title} 리뷰 추출을 시작합니다.")
+            try:
+                url = (f"https://pedia.watcha.com/ko-KR/search?query={title}")
+                driver.get(url)
+                driver.implicitly_wait(2)
+                logging.info(f"{title}으로 Watcha에 접근중")
+
+                a = driver.find_element(By.CLASS_NAME, "e1ic68ft4")
+                link = a.get_attribute("href") + "/comments?order=recent"
+                driver.get(link)
+                driver.implicitly_wait(2)
+                logging.info(f"{title}의 스크롤링을 시작합니다.")
+
+                page_scrolling(driver)
+                driver.implicitly_wait(2)
+                logging.info(f"{title}스크롤링을 마쳤습니다.")
+
+                reviews = driver.find_elements(By.CLASS_NAME, "egj9y8a4")
+                data = []
+                # 상위 200개의 리뷰만 가져오기
+                for index, review in enumerate(reviews):
+                    if index >= 200:
+                        break
+
+                    id = review.find_element(By.CLASS_NAME, "eovgsd00").text
+                    like = review.find_element(By.TAG_NAME, "em").text
+                    try:
+                        score_element = review.find_element(By.CLASS_NAME, "egj9y8a0")
+                        score = score_element.text if score_element.text != "보고싶어요" else None
+                    except NoSuchElementException:
+                        score = None
+                    comment = review.find_element(By.CLASS_NAME, "e1hvy88212").text.replace("\n", " ").replace('\"', "")
+                    # 각 리뷰의 정보를 data 리스트에 추가합니다.
+                    data.append([id, score, like, comment])
+
+                df = pd.DataFrame(data, columns=['id', 'score', 'like', 'comment'])
+                logging.info(df)
+
+                # S3에 업로드
+                upload_to_s3(title, df)
+
+            except Exception as e:
+                logging.info(f"selenium 접근 실패: {e}")
     else:
-        logging.info("No titles were retrieved.")
+        logging.info("리턴받은 제목 없음.")
+
+# 전체 페이지를 스크롤
+def page_scrolling(driver):
+    scroll_location = driver.execute_script("return document.body.scrollHeight")
+    cnt = 0
+    try:
+        while True:
+            cnt += 1
+            #현재 스크롤의 가장 아래로 내림
+            driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+
+            #전체 스크롤이 늘어날 때까지 대기
+            time.sleep(10)
+
+            #늘어난 스크롤 높이
+            scroll_height = driver.execute_script("return document.body.scrollHeight")
+
+            #늘어난 스크롤 위치와 이동 전 위치 같으면(더 이상 스크롤이 늘어나지 않으면) 종료
+            if scroll_location == scroll_height:
+                break
+
+            #같지 않으면 스크롤 위치 값을 수정하여 같아질 때까지 반복
+            else:
+                #스크롤 위치값을 수정
+                scroll_location = driver.execute_script("return document.body.scrollHeight")
+            logging.info(f"스크롤링 {cnt}회")
+    except Exception as e:
+        logging.info(f"스크롤링 실패: {e}")
 
 
-# def page_scrolling(driver):
-#     scroll_location = driver.execute_script("return document.body.scrollHeight")
+# 약 200개의 리뷰가 있는 위치까지 스크롤링
+# def page_scrolling(driver, target_height=73122):
+#     cnt = 0
+#     try:
+#         while True:
+#             cnt += 1
+#             driver.execute_script("window.scrollTo(0, window.pageYOffset + 500);")
+#             driver.implicitly_wait(2)
+#             scroll_height = driver.execute_script("return document.body.scrollHeight")
+#             if scroll_height >= target_height:
+#                 break
+#             logging.info(f"스크롤링 {cnt}회")
+#     except Exception as e:
+#         logging.info(f"스크롤링 실패: {e}")
 
+
+# def scrolling(driver, target_height=73122):
 #     while True:
-#         #현재 스크롤의 가장 아래로 내림
-#         driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
-
-#         #전체 스크롤이 늘어날 때까지 대기
+#         driver.execute_script("window.scrollTo(0, window.pageYOffset + 500);")
 #         time.sleep(2)
-
-#         #늘어난 스크롤 높이
 #         scroll_height = driver.execute_script("return document.body.scrollHeight")
-
-#         #늘어난 스크롤 위치와 이동 전 위치 같으면(더 이상 스크롤이 늘어나지 않으면) 종료
-#         if scroll_location == scroll_height:
+#         if scroll_height >= target_height:
 #             break
 
-#         #같지 않으면 스크롤 위치 값을 수정하여 같아질 때까지 반복
-#         else:
-#             #스크롤 위치값을 수정
-#             scroll_location = driver.execute_script("return document.body.scrollHeight")
+
+# def access_watcha_wrapper(**kwargs):
+#     ti = kwargs['ti']
+#     titles = ti.xcom_pull(task_ids='get_titles', key='movies_title')
+#     logging.info(titles)
+#     if titles is not None:
+#         for title in titles:
+#             logging.info(f"{title} 리뷰 추출을 시작합니다.")
+#             df = access_watcha(title, **kwargs)
+#             upload_to_s3(title, df)
+#     else:
+#         logging.info("No titles were retrieved.")
 
 
-# def get_comments(title, driver):
+# def get_comments(driver):
 #     a = driver.find_element(By.CLASS_NAME, "e1ic68ft4")
 #     link = a.get_attribute("href") + "/comments?order=recent"
 #     driver.get(link)
-#     time.sleep(5)
+#     driver.implicitly_wait(5)
+#     logging.info("스크롤링을 시작합니다.")
 #     page_scrolling(driver)
-#     time.sleep(5)
-
+#     driver.implicitly_wait(5)
+#     logging.info("스크롤링을 성공했습니다.")
+#
 #     reviews = driver.find_elements(By.CLASS_NAME, "egj9y8a4")
 #     data = []
-
+#
 #     # 상위 200개의 리뷰만 가져오기
 #     for index, review in enumerate(reviews):
 #         if index >= 200:
 #             break
-
+#
 #         id = review.find_element(By.CLASS_NAME, "eovgsd00").text
 #         like = review.find_element(By.TAG_NAME, "em").text
 #         try:
@@ -129,33 +199,40 @@ def access_watcha_wrapper(**kwargs):
 #             score = score_element.text if score_element.text != "보고싶어요" else None
 #         except NoSuchElementException:
 #             score = None
-
-#         comment = review.find_element(By.CLASS_NAME, "e1hvy88212").text.replace("\n", " ").replace('\"',"")
-
+#
+#         comment = review.find_element(By.CLASS_NAME, "e1hvy88212").text.replace("\n", " ").replace('\"', "")
+#
 #         # 각 리뷰의 정보를 리스트에 추가합니다.
 #         data.append([id, score, like, comment])
-
+#
 #     # 데이터 리스트를 DataFrame으로 변환합니다.
 #     df = pd.DataFrame(data, columns=['id', 'score', 'like', 'comment'])
 #     return df
 
 
-# def upload_to_s3(df, file_name):
-#     s3_hook = S3Hook(aws_conn_id='aws_conn')
-#     try:
-#         s3_hook.load_file(
-#             file_name=file_name,
-#             key=key,
-#             bucket_name=s3_bucket_name,
-#             replace=True
-#         )
+def upload_to_s3(title, df):
+    # 파일 제목에 들어가서는 안 되는 문자 제거
+    safe_title = re.sub(r'[\\/*?:"<>|]', "", title)
+    file_name = f"{safe_title}.csv"
+    logging.info(df)
+    key = f"watcha/{file_name}"
+    s3_hook = S3Hook(aws_conn_id='aws_conn')
 
-#         csv_buffer = StringIO()
-#         df.to_csv(csv_buffer, index=False)
-#         s3_client.put_object(Body=csv_buffer.getvalue(), Bucket=bucket_name, Key=file_name)
-#         logging.info(f"File {file_name} uploaded to S3 successfully.")
-#     except Exception as e:
-#         logging.info(f"Error uploading file to S3: {e}")
+    # DataFrame을 CSV 문자열로 변환
+    csv_buffer = StringIO()
+    df.to_csv(csv_buffer, index=False)
+    csv_string = csv_buffer.getvalue()
+
+    # S3에 업로드
+    try:
+        logging.info(f"Uploading {file_name} to s3")
+        s3_hook.load_string(string_data=csv_string,
+                            key=key,
+                            bucket_name=Variable.get("s3_bucket_name"),
+                            replace=True)  # 동일한 키가 있을 경우 덮어쓰기 설정
+        logging.info(f"File {file_name} uploaded to S3 successfully.")
+    except Exception as e:
+        logging.info(f"Error uploading file to S3: {e}")
 
 
 dag = DAG(
@@ -165,43 +242,25 @@ dag = DAG(
     catchup=False,
     max_active_runs=1,
     default_args={
-        'retries': 1,
+        #'retries': 1,
         'retry_delay': timedelta(minutes=3),
     }
 )
 
-get_titles = PythonOperator(
-    task_id='get_titles',
+get_daily_box_office = PythonOperator(
+    task_id='get_daily_box_office',
     python_callable=get_titles,
     provide_context=True,
     dag=dag
 )
 
-access_watcha = PythonOperator(
-    task_id='access_watcha',
-    python_callable=access_watcha_wrapper,
+scraping_watcha = PythonOperator(
+    task_id='scraping_watcha',
+    python_callable=scraping_watcha,
     provide_context=True,
-    dag=dag
+    dag=dag,
+    execution_timeout=timedelta(minutes=15)  # 태스크의 최대 실행 시간을 15분으로 설정
 )
 
-# get_comments = PythonOperator(
-#     task_id='get_comments',
-#     python_callable=get_comments,
-#     dag=dag
-# )
 
-# upload_to_s3 = PythonOperator(
-#     task_id = 'upload_to_s3',
-#     python_callable = upload_to_s3,
-#     provide_context = True,
-#     op_kwargs = {
-#             'filename' : '/opt/airflow/dags/jumpit.csv',
-#             'key': 'path/in/s3/bucket/jobinfo_jumpit.csv',
-#             'bucket_name' : 'de-4-2-dev-bucket'
-#     },
-#     dag = dag
-# )
-
-
-get_titles >> access_watcha
-# access_watcha >> get_comments >> upload_to_s3
+get_daily_box_office >> scraping_watcha
