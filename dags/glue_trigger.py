1514c25810980b4bced032a74d1932523179cc23
dongbin <dongbin@DESKTOP-HBHBOD7>
2024-03-08 03:15:39 +0900

    fix glue_trigger.py : skip second task

--- dags/glue_trigger.py
+++ dags/glue_trigger.py
@@ -30,29 +30,29 @@ trigger_glue_job_movie = GlueJobOperator(
     aws_conn_id="aws_conn",
     region_name="ap-northeast-2",
     dag=dag,
-    retries=3,  # 재시도 횟수
-    retry_delay=timedelta(minutes=3),  # 재시도 간격
+#    retries=3,  # 재시도 횟수
+#    retry_delay=timedelta(minutes=3),  # 재시도 간격
 )
 
 # 첫 번째 Glue 작업 완료 대기
-wait_for_glue_job_movie = GlueJobSensor(
-    task_id="wait_for_glue_job_movie",
-    job_name="de-4-2-movie",
-    aws_conn_id="aws_conn",
+#wait_for_glue_job_movie = GlueJobSensor(
+#    task_id="wait_for_glue_job_movie",
+#    job_name="de-4-2-movie",
+#    aws_conn_id="aws_conn",
     #region_name="ap-northeast-2",
-    run_id="{{ task_instance.xcom_pull(task_ids='trigger_glue_job_movie', key='return_value') }}",
-    dag=dag,
-)
+#    run_id="{{ task_instance.xcom_pull(task_ids='trigger_glue_job_movie', key='return_value') }}",
+#    dag=dag,
+#)
 
 # 두 번째 Glue 작업 실행
-trigger_glue_job_directors = GlueJobOperator(
-    task_id="trigger_glue_job_directors",
-    job_name="de-4-2-kofic-movie-directors",
-    script_location="s3://aws-glue-assets-862327261051-ap-northeast-2/scripts/de-4-2-kofic-movie-directors.py",
-    aws_conn_id="aws_conn",
-    region_name="ap-northeast-2",
-    dag=dag,
-)
+#trigger_glue_job_directors = GlueJobOperator(
+#    task_id="trigger_glue_job_directors",
+#    job_name="de-4-2-kofic-movie-directors",
+#    script_location="s3://aws-glue-assets-862327261051-ap-northeast-2/scripts/de-4-2-kofic-movie-directors.py",
+#    aws_conn_id="aws_conn",
+#    region_name="ap-northeast-2",
+#    dag=dag,
+#)
 
 # 작업 흐름 정의
-trigger_glue_job_movie >> wait_for_glue_job_movie >> trigger_glue_job_directors
+#trigger_glue_job_movie >> wait_for_glue_job_movie >> trigger_glue_job_directors
