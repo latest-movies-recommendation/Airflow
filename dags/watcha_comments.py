d19f793b0e65e1a45fdf4a0bbba422c1a1565434
bbbbeee11a <bbbbeeella@jbnu.ac.kr>
2024-03-08 00:23:29 +0900

    fix: remove typo

--- dags/watcha_comments.py
+++ dags/watcha_comments.py
@@ -62,7 +62,7 @@ def setting_driver():
 def scraping_watcha(**kwargs):
     ti = kwargs["ti"]
     titles = ti.xcom_pull(task_ids="get_daily_box_office", key="movies_title")
-    codes = ti.xcom_pull(task_id="get_daily_box_office", key="movies_code")
+    codes = ti.xcom_pull(task_ids="get_daily_box_office", key="movies_code")
     logging.info(titles)
     logging.info(f"영화코드 전달 완료!: {codes}")
     driver = setting_driver()
