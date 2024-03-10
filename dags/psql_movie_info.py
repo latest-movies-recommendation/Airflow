4a71690eb2d49e658cde860275e7c259f1bec38c
dongbin <dongbin@DESKTOP-HBHBOD7>
2024-03-11 00:54:51 +0900

    fix psql_movie_info.py

--- dags/psql_movie_info.py
+++ dags/psql_movie_info.py
@@ -42,7 +42,7 @@ default_args = {
 
 # DAG 정의
 dag = DAG(
-    "psql_music_info",
+    "psql_movie_info",
     default_args=default_args,
     description="A simple DAG to manipulate PostgreSQL data",
     schedule_interval="0 0 1 * *",
